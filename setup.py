17b9dc800bb2fd0606d27dccb17247ce632098c9
oliviaeugenio <oli@eugenio.software>
2024-11-23 15:44:28 -0500

    Added a test for setting up CICD

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Swear-Gaurd',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='oliviaeugenio',
+    author_email='',
+    description=''
+)
